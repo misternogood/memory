b152cf2ea5a19054a69396e216ae918c6d5281de
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-25 12:12:45 +0900

    creatorモデル作成

---
+++ db/migrate/20221125030728_create_creators.rb
@@ -0,0 +1,10 @@
+class CreateCreators < ActiveRecord::Migration[6.0]
+  def change
+    create_table :creators do |t|
+      t.string :name,        null: false
+      t.date :birth_date,    null: false
+      t.references :user,    null: false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
