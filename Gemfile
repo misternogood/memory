50608bb67d868962f2cbfbc43887c2c49be180c9
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-25 13:02:36 +0900

    新規作者登録機能実装

--- Gemfile
+++ Gemfile
@@ -53,6 +53,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
+gem 'active_hash'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 group :production do
   gem 'pg'
--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -1,7 +1,23 @@
 class CreatorsController < ApplicationController
-  def index; end
+  def index
+  end
 
-  def new; end
+  def new
+    @creator = Creator.new
+  end
 
-  def create; end
+  def create 
+    @creator = Creator.new(creator_params)
+    if @creator.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
+
+private
+
+  def creator_params
+    params.require(:creator).permit(:name, :birth_date, :image).merge(user_id: current_user.id)
+  end
