be2489e45bd63acc5988f7415567c58d468b602f
misternogood <114906700+misternogood@users.noreply.github.com>
2022-11-30 13:42:56 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'creators#index'
-  resources :creators, only: [:index, :new, :create, :show] do
+  resources :creators, only: [:index, :new, :create, :show, :edit] do
     resources :records, only: [:new, :create]
   end
   resources :users, only: :show
