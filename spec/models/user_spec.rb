4c0b0908a1ce3d53da6be8a579478e2a31a06919
misternogood <114906700+misternogood@users.noreply.github.com>
2022-12-05 08:34:30 +0900

    ユーザーモデル単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,93 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる時' do
+    it '登録必須項目が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    end
+    context '新規登録できない時'
+    # ニックネーム
+    it 'nicknameが空では登録できない' do
+
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    # メールアドレス
+    it 'emailが空では登録できない' do
+
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it '重複したemailが存在する場合は登録できない' do
+
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+    it 'emailは@を含まないと登録できない' do
+
+      @user.email = 'tanaka.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
+    end
+    # パスワード
+    it 'passwordが空では登録できない' do
+
+      @user.password = ''
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordが5文字以下では登録できない' do
+
+      @user.password = '1111a'
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+    it 'passwordが129文字以上では登録できない' do
+      @user.password = Faker::Internet.password(min_length: 129)
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+    end
+    it 'passwordが半角英字のみでは登録できない' do
+
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+    it 'passwordが半角数字のみでは登録できない' do
+
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+    it 'passwordに全角文字を含むと登録できない' do
+
+      @user.password = '全角password'
+      @user.password_confirmation = '全角password'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+    it 'passwordとpassword_confirmationが不一致では登録できない' do
+
+      @user.password = '11111a'
+      @user.password_confirmation = '1111aa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
