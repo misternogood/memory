87dc1a9912ab37b6fe814d8f871aa5fbe9bc04c0
misternogood <114906700+misternogood@users.noreply.github.com>
2022-12-05 13:35:06 +0900

    レコードモデル単体テストコード

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @record = FactoryBot.build(:record)
+  end
+
+  describe '作者新規登録' do
+    context '新規登録できる時' do
+    it '登録必須項目が存在すれば登録できる' do
+        expect(@record).to be_valid
+      end
+    end
+
+    end
+    context '新規登録できない時'
+    # タイトル
+    it 'titleが空では登録できない' do
+      @record.title = ''
+      @record.valid?
+      expect(@record.errors.full_messages).to include("Title can't be blank")
+    end
+    # 説明
+    it 'contentが空では登録できない' do
+      @record.content = ''
+      @record.valid?
+      expect(@record.errors.full_messages).to include ("Content can't be blank")
+    end
+    # 画像
+    it 'imageが空では登録できない' do
+      @record.image = nil
+      @record.valid?
+      expect(@record.errors.full_messages).to include("Image can't be blank")
+    end
+    # 作者
+    it 'creatorが紐づいていなければ登録できない' do
+      @record.creator = nil
+      @record.valid?
+      expect(@record.errors.full_messages).to include('Creator must exist')
+    end
+  end
