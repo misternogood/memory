08fc4a28ff5b112f26442764e89f727e5ec57cfa
misternogood <114906700+misternogood@users.noreply.github.com>
2022-12-14 10:57:00 +0900

    お気に入り機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'creators#index'
   resources :creators do
-    resources :records, only: [:new, :create, :show, :edit, :update, :destroy]
+    resources :records, only: [:new, :create, :show, :edit, :update, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+    end
   end
   resources :users, only: :show
 end
